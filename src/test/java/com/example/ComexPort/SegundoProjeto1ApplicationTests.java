1bd15f6f442327288125af8d204551301f8450ff
Fabio Aoki <fabio.aoki.93@gmail.com>
2020-07-24 14:57:55 -0300

    initial commit.

---
+++ src/test/java/com/example/ComexPort/SegundoProjeto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ComexPort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoProjeto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
